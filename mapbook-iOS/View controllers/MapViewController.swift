22ae228c2d4a809f1b668b6827f78fb99cb5c4d8
Eli Gregory <egregory@esri.com>
2018-10-29 10:08:30 -0700

    selectionWidth will be deprecated.

--- mapbook-iOS/View controllers/MapViewController.swift
+++ mapbook-iOS/View controllers/MapViewController.swift
@@ -61,15 +61,6 @@ class MapViewController: UIViewController {
         self.overlayView.layer.borderColor = UIColor.lightGray.withAlphaComponent(0.3).cgColor
         self.overlayView.layer.borderWidth = 2
         
-        //increase selection width for feature layers
-        if let operationalLayers = self.map?.operationalLayers as? [AGSLayer] {
-            _ = operationalLayers.map ({
-                if let featureLayer = $0 as? AGSFeatureLayer {
-                    featureLayer.selectionWidth = 5
-                }
-            })
-        }
-        
         //hide side panel by default
         self.toggleOverlay(on: false, animated: false)
     }
